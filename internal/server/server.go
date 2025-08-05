034adf8df0cfff3fbed50c1f82a67b28a348fe44
isadri <abkadriissam@gmail.com>
2025-08-05 18:53:33 +0100

    feat: list all organization repos

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,31 +6,48 @@ import (
 	"net/http"
 
 	"github.com/isadri/cicd-dashboard/internal/gitactions"
+	"github.com/isadri/cicd-dashboard/internal/repos"
 )
 
 func RegisterFuncs() {
 	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/customer", customerHandler)
+	http.HandleFunc("/repo", repoHandler)
 }
 
 func homeHandler(w http.ResponseWriter, req *http.Request) {
 	log.Printf("%s %s from %s", req.Method, req.URL.Path, req.RemoteAddr)
+
+	log.Print("get fasgo-app repositories")
+	repos, err := repos.GetRepos("fasgo-app")
+	if err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
 	log.Print("creating home.index template")
 	templ := template.Must(template.New("home.html").
 		ParseFiles("web/template/home.html"))
 	log.Print("executing home.html template")
-	if err := templ.Execute(w, nil); err != nil {
+	if err := templ.Execute(w, repos); err != nil {
 		log.Print(err)
 		w.WriteHeader(http.StatusBadRequest)
 	}
 }
 
-func customerHandler(w http.ResponseWriter, req *http.Request) {
+func repoHandler(w http.ResponseWriter, req *http.Request) {
 	log.Printf("%s %s from %s", req.Method, req.URL.Path, req.RemoteAddr)
 
 	var workflows *gitactions.Workflows
 
-	workflows, err := gitactions.GetWorkflows("fasgo-app", "Fasgo_Customer")
+	repoName := req.URL.Query().Get("name")
+	if repoName == "" {
+		log.Print("repository name path parameter is required")
+		w.Write([]byte("repository name path parameter is required"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	workflows, err := gitactions.GetWorkflows("fasgo-app", repoName)
 	if err != nil {
 		log.Print(err)
 		w.WriteHeader(http.StatusBadRequest)
