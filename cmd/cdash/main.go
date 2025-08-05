1b68d0540877bf11957f8637329da4cd62a31b5b
isadri <abkadriissam@gmail.com>
2025-08-05 13:31:11 +0100

    feat: run server

--- cmd/cdash/main.go
+++ cmd/cdash/main.go
@@ -1,30 +1,42 @@
 package main
 
 import (
-	// "encoding/json"
-	// "fmt"
 	"html/template"
 	"log"
-	"os"
+	"net/http"
 
 	"github.com/isadri/cicd-dashboard/internal/gitactions"
 
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+func run(w http.ResponseWriter, r *http.Request) {
+	log.Printf("+ accept connection %s", r.RemoteAddr)
 	var workflows *gitactions.Workflows
+	var err error
 
 	if workflows, err = gitactions.GetWorkflows("isadri", "eval"); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Print("create dashboard.html template")
 	templ := template.Must(template.New("dashboard.html").
 		ParseFiles("web/templates/dashboard.html"))
-	if err := templ.Execute(os.Stdout, workflows); err != nil {
-		log.Fatal(err)
+	log.Print("execute dashboard.html template")
+	if err := templ.Execute(w, workflows); err != nil {
+		log.Print(err)
+		w.WriteHeader(http.StatusBadRequest)
 	}
+}
+
+func main() {
+	log.Print("loading environment variables in .env")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	http.HandleFunc("/", run)
+	log.Print("start server")
+	log.Print("server listening on localhost:8000")
+	http.ListenAndServe("localhost:8000", nil)
 }
